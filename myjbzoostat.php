9b94ba6b089b94034aafd0772223aecc52caddf4
CB9TOIIIA <cb9toiiia@gmail.com>
2016-08-27 01:40:38 +0300

    update

--- myjbzoostat.php
+++ myjbzoostat.php
@@ -4,10 +4,8 @@ defined( '_JEXEC' ) or die; // No direct access
  * Component myjbzoostat
  * @author CB9TOIIIA
  */
-$mainframe = JFactory::getApplication();
-$namecomponent = $mainframe->scope;
-$namecomponent = str_replace('com_','',$namecomponent);
-require_once JPATH_COMPONENT.'/helpers/'.$namecomponent.'.php';
-$controller = JControllerLegacy::getInstance( $namecomponent );
+
+require_once JPATH_COMPONENT.'/helpers/myjbzoostat.php';
+$controller = JControllerLegacy::getInstance('myjbzoostat');
 $controller->execute( JFactory::getApplication()->input->get( 'task' ) );
 $controller->redirect();
